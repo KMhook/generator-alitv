fefbe02aff7957bbfd780fabd55cadba900bd479
hukuanmin <kuanmin.hkm@alibaba-inc.com>
2016-04-15 14:45:55 +0800

    1. new: add dependencies for mocha, jsdom, etc;

--- generators/main/lib.js
+++ generators/main/lib.js
@@ -5,62 +5,10 @@ var mkdirp = require('mkdirp');
 var templates = require('./main').templates;
 
 module.exports = {
-  ask: function() {
-    var cb = this.async();
-
-    var prompts = [
-      {
-        type: 'checkbox',
-        name: 'modules',
-        message: 'Which modules would you like to include?',
-        choices: [
-          {
-            value: 'react',
-            name: 'react.js',
-            checked: true
-          },
-          {
-            value: 'es5sham',
-            name: 'es5sham-min.js',
-            checked: true
-          },
-          {
-            value: 'es5shim',
-            name: 'es5shim-min.js',
-            checked: true
-          },
-          {
-            value: 'console',
-            name: 'console-polyfill.js',
-            checked: true
-          },
-          {
-            value: 'redux',
-            name: 'redux-min.js',
-            checked: true
-          }
-        ]
-      }
-    ];
-
-    this.prompt(prompts, function (props) {
-      var hasMod = function (mod) {
-        return props.modules.indexOf(mod) !== -1;
-      };
-      this.react = hasMod('react');
-      this.es5sham = hasMod('es5sham');
-      this.es5shim = hasMod('es5shim');
-      this.console = hasMod('console');
-      this.redux = hasMod('redux');
-
-      cb();
-    }.bind(this));
-  },
-
-  create: function() {
-    mkdirp('src/lib');
-    mkdirp('src/lib/css');
-    mkdirp('src/lib/js');
+  /*
+   * copy common libs to projects
+   */
+  _createCommonLib: function() {
     this.fs.copy(
       this.templatePath(templates+'lib/css/reset.css'),
       this.destinationPath('src/lib/css/reset.css')
@@ -81,12 +29,76 @@ module.exports = {
       this.templatePath(templates+'lib/js/get-min.js'),
       this.destinationPath('src/lib/js/get-min.js')
     );
+  },
 
-    if (this.react) {
+  ask: function() {
+    var cb = this.async();
+
+    var prompts = [
+      {
+        type: 'checkbox',
+        name: 'modules',
+        message: 'Which modules would you like to include?',
+        choices: [
+          {
+            value: 'reactWithRedux',
+            name: 'react.js + redux.js',
+            checked: true
+          },
+          {
+            value: 'es5sham',
+            name: 'es5sham-min.js',
+            checked: true
+          },
+          {
+            value: 'es5shim',
+            name: 'es5shim-min.js',
+            checked: true
+          },
+          {
+            value: 'console',
+            name: 'console-polyfill.js',
+            checked: true
+          }
+        ]
+      }
+    ];
+
+    this.prompt(prompts, function (props) {
+      var hasMod = function (mod) {
+        return props.modules.indexOf(mod) !== -1;
+      };
+      this.reactWithRedux = hasMod('reactWithRedux');
+      this.es5sham = hasMod('es5sham');
+      this.es5shim = hasMod('es5shim');
+      this.console = hasMod('console');
+      //this.redux = hasMod('redux');
+
+      cb();
+    }.bind(this));
+  },
+
+  create: function() {
+    mkdirp('src/lib');
+    mkdirp('src/lib/css');
+    mkdirp('src/lib/js');
+    this._createCommonLib();
+    
+    if (this.reactWithRedux) {
       this.fs.copy(
         this.templatePath(templates+'lib/js/react.js'),
         this.destinationPath('src/lib/js/react.js')
       );
+      this.fs.copy(
+        this.templatePath(templates+'lib/js/redux-min.js'),
+        this.destinationPath('src/lib/js/redux-min.js')
+      );
+      this.fs.copy(
+        this.templatePath(templates+'lib/js/react-redux-min.js'),
+        this.destinationPath('src/lib/js/react-redux-min.js')
+      );
+
+      this.config.set('fluxFramework', 'redux');
       this.config.set('react', true);
     }
 
@@ -111,17 +123,5 @@ module.exports = {
       );
     }
 
-    if (this.redux) {
-      this.fs.copy(
-        this.templatePath(templates+'lib/js/redux-min.js'),
-        this.destinationPath('src/lib/js/redux-min.js')
-      );
-
-      this.fs.copy(
-        this.templatePath(templates+'lib/js/react-redux-min.js'),
-        this.destinationPath('src/lib/js/react-redux-min.js')
-      );
-      this.config.set('fluxFramework', 'redux');
-    }
   }
 };
